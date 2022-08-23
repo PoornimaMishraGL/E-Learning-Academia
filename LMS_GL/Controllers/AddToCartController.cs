b3d202726d07e3117b28107cba3981e82a1e0cbb
PoornimaMishraGL <km.mishra@globallogic.com>
2022-08-23 09:47:23 +0530

    #migration and View Poornima

---
+++ LMS_GL/Controllers/AddToCartController.cs
@@ -0,0 +1,36 @@
+﻿using LMS_GL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_GL.Controllers
+{
+    public class AddToCartController : Controller
+    {
+        public ActionResult Add(Courses mo)
+        {
+
+            if (Session["cart"] == null)
+            {
+                List<Courses> li = new List<Courses>();
+
+                li.Add(mo);
+                Session["cart"] = li;
+                ViewBag.cart = li.Count();
+
+
+                Session["count"] = 1;
+
+
+            }
+            else
+            {
+                List<Courses> li = (List<Courses>)Session["cart"];
+                li.Add(mo);
+                Session["cart"] = li;
+                ViewBag.cart = li.Count();
+                Session["count"] = Convert.ToInt32(Session["count"]) + 1;
+
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
