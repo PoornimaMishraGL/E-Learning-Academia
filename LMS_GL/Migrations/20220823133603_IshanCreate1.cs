182b1f3bf8e4eabd618b52b4345e8f31c3a97f14
IshanTrisalGL <ishan.trisal@globallogic.com>
2022-08-23 19:10:54 +0530

    MIgration ishan

---
+++ LMS_GL/Migrations/20220823133603_IshanCreate1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_GL.Migrations
+{
+    public partial class IshanCreate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ LMS_GL/Migrations/ApplicationDb/20220823133511_IshanCreate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_GL.Migrations.ApplicationDb
+{
+    public partial class IshanCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
